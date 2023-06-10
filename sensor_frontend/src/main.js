9600922df4d90de31beb61e4f1ee97c33052459c
Nasreddin Elalawani <nasreddinelalawani@AirvonNasreddin.fritz.box>
2023-06-10 21:18:03 +0200

    add icons

--- sensor_frontend/src/main.js
+++ sensor_frontend/src/main.js
@@ -7,6 +7,31 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios";
 
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {
+    faInfo, faArrowRight, faSun, faMoon, faBars, faUser, faHouseChimney, faUsers, faMessage, faStethoscope,
+    faEnvelope, faFemale, faMale, faSearch, faLungs, faTemperatureHalf, faHeartbeat, faDroplet, faPaperPlane,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+    faTwitter, faYoutube, faLinkedin, faGithub
+} from "@fortawesome/free-brands-svg-icons";
+
+
+/* add icons to the library */
+library.add(
+    faSearch, faMoon, faBars, faSun, faArrowRight, faInfo, faUser, faUsers, faLinkedin,
+    faStethoscope, faEnvelope, faMessage, faHouseChimney, faFemale, faMale, faYoutube, faTwitter, faGithub, faLungs,
+    faTemperatureHalf, faHeartbeat, faDroplet, faPaperPlane
+)
+
+
 // 8000 port witch django uses
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
@@ -14,5 +39,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router, axios)
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 
 app.mount('#app')
