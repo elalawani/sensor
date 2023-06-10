21c656115a1ac35861f545b666bbb858a3f2c750
Nasreddin Elalawani <nasreddinelalawani@AirvonNasreddin.fritz.box>
2023-06-10 22:58:36 +0200

    add login page

--- sensor_frontend/src/App.vue
+++ sensor_frontend/src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <RouterView />
+  <div id="app">
+      <navbar/>
+  </div>
+  <router-view/>
 </template>
+<script>
+import navbar from "../src/components/navbar.vue";
+export default {
+    name: 'app',
+    components: {
+        navbar
+    }
+}
+</script>
--- sensor_frontend/src/views/DashboardView.vue
+++ sensor_frontend/src/views/DashboardView.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-      test
+      dashboard
   </div>
 </template>
