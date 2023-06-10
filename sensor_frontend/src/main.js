54c60eb7950b52fe5944365454affdcb4acafdbd
Nasreddin Elalawani <nasreddinelalawani@AirvonNasreddin.fritz.box>
2023-06-10 19:37:57 +0200

    install axios

--- sensor_frontend/src/main.js
+++ sensor_frontend/src/main.js
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+
+// 8000 port witch django uses
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router, axios)
 
 app.mount('#app')
