d8e5e02732aa2ee8e84857bae37d9f46f611b8af
Nasreddin Elalawani <nasreddinelalawani@AirvonNasreddin.fritz.box>
2023-06-11 09:58:05 +0200

    update home page

--- sensor_frontend/src/views/DashboardView.vue
+++ sensor_frontend/src/views/DashboardView.vue
@@ -1,94 +1,53 @@
 <template>
-    <div class="flex sm:flex-row flex-col justify-around mt-14">
-        <div class="mt-10 mx-5 w-full">
-        <router-link to="/">
-            <div class="flex items-center hover:py-9 p-8 bg-gray-50 shadow-2xl border hover:border-sky-800 rounded-lg mb-2
-            transition-all duration-300 ease-linear relative">
-                <i class="py-4 px-5 text-gray-900 bg-slate-200 rounded-full text-4xl">
-                    <font-awesome-icon icon="fa-solid fa-user" />
-                </i>
-                <div class="text-slate-900 p-8">
-                    <strong>my profile</strong><br>
-                    <span class="text-gray-400">geborn am: 12.12.2000</span>
-                </div>
-                <div class="bg-sky-800 absolute px-3 py-1 bottom-[-1px] right-[-1px] text-gray-50 rounded-br-lg rounded-tl-lg">
-                    <font-awesome-icon icon="fa-solid fa-arrow-right" />
-                </div>
-            </div>
-        </router-link>
-    </div>
-        <div class="mt-10 mx-5 w-full">
-        <router-link to="/">
-            <div class="flex items-center hover:py-9 p-8 bg-gray-50 shadow-2xl border hover:border-sky-800 rounded-lg mb-2
-            transition-all duration-300 ease-linear relative">
-                <i class="py-4 px-5 text-gray-900 bg-slate-200 rounded-full text-4xl">
-                    <font-awesome-icon icon="fa-solid fa-user" />
-                </i>
-                <div class="text-slate-900 p-8">
-                    <strong>notifications</strong><br>
-                    <span class="text-gray-400">geborn am: 12.12.2000</span>
-                </div>
-                <div class="bg-sky-800 absolute px-3 py-1 bottom-[-1px] right-[-1px] text-gray-50 rounded-br-lg rounded-tl-lg">
-                    <font-awesome-icon icon="fa-solid fa-arrow-right" />
+    <div class="mt-24 mx-4">
+        <div> hi <span> nurse </span></div>
+
+        <div class="flex flex-col sm:flex-row justify-around mt-20">
+            <div class="w-2/5 mb-4 sm-mb-0">
+                <div class="flex justify-between">
+                    <span>
+                    my Todos
+                    </span>
+                    <router-link to="/">
+                        all
+                    </router-link>
+                    </div>
+                <div>
+                    <hr>
+                    <ul class="list">
+                        <li class="my-3">
+                            <input type="checkbox" name="" id="">
+                            <span> meeting with doctor </span>
+                        </li>
+                        <li class="mb-3">
+                            <input type="checkbox" name="" id="">
+                            <span> write a Documentation </span>
+                        </li>
+                        <li class="mb-3">
+                            <input type="checkbox" name="" id="">
+                            <span> check around before i go</span>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </router-link>
-    </div>
-    </div>
-    <div class="flex sm:flex-row flex-col justify-evenly">
-        <div class="mt-10 mx-5 w-full">
-        <router-link to="/">
-            <div class="flex items-center hover:py-9 p-8 bg-gray-50 shadow-2xl border hover:border-sky-800 rounded-lg mb-2
-            transition-all duration-300 ease-linear relative">
-                <i class="py-4 px-5 text-gray-900 bg-slate-200 rounded-full text-4xl">
-                    <font-awesome-icon icon="fa-solid fa-user" />
-                </i>
-                <div class="text-slate-900 p-8">
-                    <strong>my Todos</strong><br>
-                    <span class="text-gray-400">geborn am: 12.12.2000</span>
-                </div>
-                <div class="bg-sky-800 absolute px-3 py-1 bottom-[-1px] right-[-1px] text-gray-50 rounded-br-lg rounded-tl-lg">
-                    <font-awesome-icon icon="fa-solid fa-arrow-right" />
+            <div class="w-2/5 sm:ml-8">
+                <span>
+                    my Todos
+                </span>
+                <span>
+                    all
+                </span>
+                <hr>
+                <div>
+                    <ul>
+                        <li>
+                            meeting with doctor
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </router-link>
-    </div>
-        <div class="mt-10 mx-5 w-full">
-        <router-link to="/">
-            <div class="flex items-center hover:py-9 p-8 bg-gray-50 shadow-2xl border hover:border-sky-800 rounded-lg mb-2
-            transition-all duration-300 ease-linear relative">
-                <i class="py-4 px-5 text-gray-900 bg-slate-200 rounded-full text-4xl">
-                    <font-awesome-icon icon="fa-solid fa-user" />
-                </i>
-                <div class="text-slate-900 p-8">
-                    <strong>last measurement</strong><br>
-                    <span class="text-gray-400">geborn am: 12.12.2000</span>
-                </div>
-                <div class="bg-sky-800 absolute px-3 py-1 bottom-[-1px] right-[-1px] text-gray-50 rounded-br-lg rounded-tl-lg">
-                    <font-awesome-icon icon="fa-solid fa-arrow-right" />
-                </div>
-            </div>
-        </router-link>
-    </div>
-    </div>
-    <div class="flex sm:flex-row flex-col justify-evenly">
-        <div class="mt-10 mx-5 w-full">
-        <router-link to="/">
-            <div class="flex items-center hover:py-9 p-8 bg-gray-50 shadow-2xl border hover:border-sky-800 rounded-lg mb-2
-            transition-all duration-300 ease-linear relative">
-                <i class="py-4 px-5 text-gray-900 bg-slate-200 rounded-full text-4xl">
-                    <font-awesome-icon icon="fa-solid fa-user" />
-                </i>
-                <div class="text-slate-900 p-8">
-                    <strong>Search for a patient</strong><br>
-                    <span class="text-gray-400">geborn am: 12.12.2000</span>
-                </div>
-                <div class="bg-sky-800 absolute px-3 py-1 bottom-[-1px] right-[-1px] text-gray-50 rounded-br-lg rounded-tl-lg">
-                    <font-awesome-icon icon="fa-solid fa-arrow-right" />
-                </div>
-            </div>
-        </router-link>
-    </div>
+
+        </div>
     </div>
 </template>
 <script setup>
