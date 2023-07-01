88a52e147419799177663f9b1e241dfd388dd2d4
Nasreddin Elalawani <nasreddinelalawani@AirvonNasreddin.fritz.box>
2023-07-01 15:49:22 +0200

    add models and serializers

--- sensor_backend/sensor/models.py
+++ sensor_backend/sensor/models.py
@@ -5,34 +5,104 @@ from patient.models import Patient
 
 
 class Measurement(models.Model):
-    comment = models.TextField(null=True)
-    rating = models.PositiveSmallIntegerField(null=True, blank=True,
-                                              validators=[MinValueValidator(1), MaxValueValidator(5)])
-    created_by = models.ForeignKey(User, related_name='creator', on_delete=models.CASCADE)
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='%(class)s_creator', on_delete=models.CASCADE, null=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=30, null=True)
+    code = models.CharField(max_length=255, null=True)
+    category = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class HeartRateMeasurement(Measurement):
+    HeartRate = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=30, default='mg/dL')
+
+
+class HeartRateMeasurementComments(models.Model):
+    text = models.TextField()
+    measurement = models.ForeignKey(HeartRateMeasurement, related_name='heart_rate_comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='heart_rate_comments_creator', on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class HeartRateMeasurement(models.Model):
-    HeartRate = models.FloatField(null=True, blank=True)
-
-
-class BloodPressureMeasurement(models.Model):
+class BloodPressureMeasurement(Measurement):
     systolic_Pressure = models.FloatField(null=True, blank=True)
     diastolic_Pressure = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=30, default='mg/dL')
 
 
-class BloodGlucoseMeasurement(models.Model):
+class BloodPressureMeasurementComments(models.Model):
+    text = models.TextField()
+    measurement = models.ForeignKey(BloodPressureMeasurement, related_name='blood_pressure_comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='blood_pressure_comments_creator', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class BloodGlucoseMeasurement(Measurement):
     BloodGlucose = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=30, default='mg/dL')
 
 
-class RespirationRateMeasurement(models.Model):
+class BloodGlucoseMeasurementComments(models.Model):
+    text = models.TextField()
+    measurement = models.ForeignKey(BloodGlucoseMeasurement, related_name='blood_glucose_comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='blood_glucose_comments_creator', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class RespirationRateMeasurement(Measurement):
     RespirationRate = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=30, default='mg/dL')
 
 
-class TemperatureMeasurement(models.Model):
+class RespirationRateMeasurementComments(models.Model):
+    text = models.TextField()
+    measurement = models.ForeignKey(RespirationRateMeasurement, related_name='respiration_rate_comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='respiration_rate_comments_creator', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class TemperatureMeasurement(Measurement):
     Temperature = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=30, default='mg/dL')
 
 
-class BloodCholesterolMeasurement(models.Model):
+class TemperatureMeasurementComments(models.Model):
+    text = models.TextField()
+    measurement = models.ForeignKey(TemperatureMeasurement, related_name='Temperature_comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='Temperature_comments_creator', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class BloodCholesterolMeasurement(Measurement):
     BloodCholesterol = models.FloatField(null=True, blank=True)
+    unit = models.CharField(max_length=30, default='mg/dL')
+
+
+class BloodCholesterolMeasurementComments(models.Model):
+    text = models.TextField()
+    measurement = models.ForeignKey(BloodCholesterolMeasurement, related_name='blood_cholesterol_comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='blood_cholesterol_comments_creator', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ParkinsonsMeasurement(Measurement):
+    TREMOR_SEVERITY_CHOICES = [
+        (1, 'Slight'),
+        (2, 'Noticeable'),
+        (3, 'Moderate'),
+        (4, 'Severe'),
+    ]
+    tremor_severity = models.IntegerField(choices=TREMOR_SEVERITY_CHOICES)
+    gait_speed = models.FloatField()
+    daily_assessment = models.IntegerField()
+
+
+class ParkinsonsMeasurementComments(models.Model):
+    text = models.TextField()
+    measurement = models.ForeignKey(ParkinsonsMeasurement, related_name='parkinson_comments', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='parkinson_comments_creator', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
--- sensor_backend/sensor/serializers.py
+++ sensor_backend/sensor/serializers.py
@@ -5,40 +5,108 @@ from .models import *
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
-        fields = ['nurse', 'patient', 'timestamp', 'comment', 'rating']
+        fields = ['created_by', 'created_at', 'status', 'patient', 'code', 'category']
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ['text', 'created_by', 'created_at']
+        abstract = True
+
+
+class ParkinsonsMeasurementSerializer(MeasurementSerializer):
+    class Meta(MeasurementSerializer.Meta):
+        model = ParkinsonsMeasurement
+        fields = MeasurementSerializer.Meta.fields + ['tremor_severity', 'gait_speed', 'daily_assessment']
+
+
+class ParkinsonsMeasurementCommentSerializer(CommentsSerializer):
+    measurement = ParkinsonsMeasurementSerializer()
+
+    class Meta:
+        model = ParkinsonsMeasurementComments
+        fields = CommentsSerializer.Meta.fields + ['measurement']
 
 
 class BloodGlucoseMeasurementSerializer(MeasurementSerializer):
     class Meta(MeasurementSerializer.Meta):
         model = BloodGlucoseMeasurement
-        fields = MeasurementSerializer.Meta.fields + ['BloodGlucose']
+        fields = MeasurementSerializer.Meta.fields + ['BloodGlucose', 'unit']
+
+
+class BloodGlucoseMeasurementCommentSerializer(CommentsSerializer):
+    measurement = BloodGlucoseMeasurementSerializer()
+
+    class Meta:
+        model = BloodGlucoseMeasurement
+        fields = CommentsSerializer.Meta.fields + ['measurement']
 
 
 class BloodPressureMeasurementSerializer(MeasurementSerializer):
     class Meta(MeasurementSerializer.Meta):
         model = BloodPressureMeasurement
-        fields = MeasurementSerializer.Meta.fields + ['systolic_Pressure', 'diastolic_Pressure']
+        fields = MeasurementSerializer.Meta.fields + ['systolic_Pressure', 'diastolic_Pressure', 'unit']
+
+
+class BloodPressureMeasurementCommentSerializer(CommentsSerializer):
+    measurement = BloodPressureMeasurementSerializer()
+
+    class Meta:
+        model = BloodPressureMeasurement
+        fields = CommentsSerializer.Meta.fields + ['measurement']
 
 
 class HeartRateMeasurementSerializer(MeasurementSerializer):
     class Meta(MeasurementSerializer.Meta):
         model = HeartRateMeasurement
-        fields = MeasurementSerializer.Meta.fields + ['HeartRate']
+        fields = MeasurementSerializer.Meta.fields + ['HeartRate', 'unit']
+
+
+class HeartRateMeasurementCommentSerializer(CommentsSerializer):
+    measurement = HeartRateMeasurementSerializer()
+
+    class Meta:
+        model = HeartRateMeasurement
+        fields = CommentsSerializer.Meta.fields + ['measurement']
 
 
 class RespirationRateMeasurementSerializer(MeasurementSerializer):
     class Meta(MeasurementSerializer.Meta):
         model = RespirationRateMeasurement
-        fields = MeasurementSerializer.Meta.fields + ['RespirationRate']
+        fields = MeasurementSerializer.Meta.fields + ['RespirationRate', 'unit']
+
+
+class RespirationRateMeasurementCommentSerializer(CommentsSerializer):
+    measurement = RespirationRateMeasurementSerializer()
+
+    class Meta:
+        model = RespirationRateMeasurement
+        fields = CommentsSerializer.Meta.fields + ['measurement']
 
 
 class TemperatureMeasurementSerializer(MeasurementSerializer):
     class Meta(MeasurementSerializer.Meta):
         model = TemperatureMeasurement
-        fields = MeasurementSerializer.Meta.fields + ['Temperature']
+        fields = MeasurementSerializer.Meta.fields + ['Temperature', 'unit']
+
+
+class TemperatureMeasurementCommentSerializer(CommentsSerializer):
+    measurement = TemperatureMeasurementSerializer()
+
+    class Meta:
+        model = TemperatureMeasurement
+        fields = CommentsSerializer.Meta.fields + ['measurement']
 
 
 class BloodCholesterolMeasurementSerializer(MeasurementSerializer):
     class Meta(MeasurementSerializer.Meta):
         model = BloodCholesterolMeasurement
-        fields = MeasurementSerializer.Meta.fields + ['BloodCholesterol']
+        fields = MeasurementSerializer.Meta.fields + ['BloodCholesterol', 'unit']
+
+
+class BloodCholesterolMeasurementCommentSerializer(CommentsSerializer):
+    measurement = BloodCholesterolMeasurementSerializer()
+
+    class Meta:
+        model = BloodCholesterolMeasurement
+        fields = CommentsSerializer.Meta.fields + ['measurement']
